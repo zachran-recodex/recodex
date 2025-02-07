88eefb7f5473fadd8f97d40c63cc87781ac0bcd9
Zachran Razendra <zachranraze@recodex.id>
2025-02-08 01:13:26 +0700

    Remove xterm dependency and related routes from vendor update functionality

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\VendorUpdateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,10 +14,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard.index');
     })->name('dashboard');
-
-    Route::get('/vendor-update', [VendorUpdateController::class, 'index'])
-        ->name('vendor.update');
-
-    Route::post('/vendor-update', [VendorUpdateController::class, 'update'])
-        ->name('vendor.update.execute');
 });
