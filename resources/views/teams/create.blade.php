be05ff3208cb3eded3c7b01382f5ce3629f43a4c
Zachran Razendra <zachranraze@recodex.id>
2025-02-07 23:02:12 +0700

    Refactor layout components to use <x-dashboard-layout> for consistency across views

--- resources/views/teams/create.blade.php
+++ resources/views/teams/create.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-dashboard-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Create Team') }}
@@ -10,4 +10,4 @@
             @livewire('teams.create-team-form')
         </div>
     </div>
-</x-app-layout>
+</x-dashboard-layout>
--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-dashboard-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Team Settings') }}
@@ -20,4 +20,4 @@
             @endif
         </div>
     </div>
-</x-app-layout>
+</x-dashboard-layout>
