dfe89d9cd04c54d611f86fecf1876e3506d9a168
Zachran Razendra <zachranraze@recodex.id>
2025-03-11 16:33:54 +0700

    Update app logo and text to reflect new branding

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    <x-app-logo-icon class="size-6" />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold text-primary-500">Recodex ID</span>
 </div>
