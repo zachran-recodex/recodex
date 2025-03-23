9bd6909fe8224c46f52047e66c45c0f70f0a2c19
Zachran Razendra <zachranraze@recodex.id>
2025-03-23 18:06:27 +0700

    refactor: remove Debugbar alias registration
    The Debugbar alias registration was removed as it is no longer needed in the application. This simplifies the AppServiceProvider and reduces unnecessary code.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,8 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-        $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
+        //
     }
 
     /**
