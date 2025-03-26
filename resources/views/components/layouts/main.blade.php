a01602931a1a46c4c25dff381bb1211e7c50a5f4
Zachran Razendra <168270700+zachran-recodex@users.noreply.github.com>
2025-03-26 06:52:20 +0000

    feat(favicon): add multiple favicon sizes and web manifest for improved site branding

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -12,7 +12,11 @@
     <title>{{ config('app.name') }}</title>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
     <!-- Sitefont -->
     <link rel="stylesheet" href="{{ asset('fonts/webfonts/inter/stylesheet.css') }}" />
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,12 @@
 
 <title>Dashboard | Recodex ID</title>
 
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+<link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
