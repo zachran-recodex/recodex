19edfb0bd6ee8543d2f6ba324d0222e6991f3bf1
Zachran Razendra <zachranraze@recodex.id>
2025-04-07 19:33:27 +0700

    refactor(views): update head section reference in auth layout
    - Changed the head section inclusion in the auth layout to reference the new partial view for improved organization and maintainability.

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
-        @include('partials.head')
+        @include('components.layouts.partials.head')
     </head>
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
