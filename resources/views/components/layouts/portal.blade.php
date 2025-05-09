16fdef708344560afae58e8409496094f0af76c5
Zachran Razendra <zachranraze@recodex.id>
2025-05-09 08:27:17 +0700

    refactor(portal): clean up PortalController and enhance portal layout
    Remove unused methods in PortalController to improve maintainability. Introduce a new portal layout component with improved navigation and meta tags for better SEO and user experience.

---
+++ resources/views/components/layouts/portal.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+    <head>
+        @include('components.partials.head')
+
+        @yield('meta_tag')
+
+        <title>{{ $title ?? config('app.name') }}</title>
+    </head>
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:sidebar sticky stashable class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+
+            <a href="{{ route('dashboard') }}" class="me-5 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
+                <x-app-logo />
+            </a>
+
+            <flux:navlist.group class="grid">
+                <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>Dashboard</flux:navlist.item>
+
+                <flux:navlist variant="outline">
+                    <flux:navlist.group expandable heading="Administrator" class="grid">
+                        <flux:navlist.item icon="users" :href="route('admin.users')" :current="request()->routeIs('admin.users')" badge="fix" wire:navigate>Manage Users</flux:navlist.item>
+                    </flux:navlist.group>
+                </flux:navlist>
+
+                <flux:navlist variant="outline">
+                    <flux:navlist.group expandable heading="Content Management" class="grid">
+                        <flux:navlist.item icon="document-text" :href="route('cms.faqs')" :current="request()->routeIs('cms.faqs')" wire:navigate>FAQs</flux:navlist.item>
+                        <flux:navlist.item icon="briefcase" :href="route('cms.services')" :current="request()->routeIs('cms.services')" wire:navigate>Services</flux:navlist.item>
+                        <flux:navlist.item icon="users" :href="route('cms.members')" :current="request()->routeIs('cms.members')" wire:navigate>Team Members</flux:navlist.item>
+                        <flux:navlist.item icon="currency-dollar" :href="route('cms.pricings')" :current="request()->routeIs('cms.pricings')" wire:navigate>Pricing Packages</flux:navlist.item>
+                        <flux:navlist.item icon="arrow-path" :href="route('cms.work-processes')" :current="request()->routeIs('cms.work-processes')" wire:navigate>Work Processes</flux:navlist.item>
+                        <flux:navlist.item icon="information-circle" :href="route('cms.about')" :current="request()->routeIs('cms.about')" wire:navigate>About</flux:navlist.item>
+                        <flux:navlist.item icon="flag" :href="route('cms.hero')" :current="request()->routeIs('cms.hero')" wire:navigate>Hero</flux:navlist.item>
+                    </flux:navlist.group>
+                </flux:navlist>
+
+                <flux:navlist variant="outline">
+                    <flux:navlist.group expandable heading="Project Management" class="grid">
+                        <flux:navlist.item icon="squares-2x2" :href="route('pm.projects')" :current="request()->routeIs('pm.projects')" wire:navigate>Projects</flux:navlist.item>
+                        <flux:navlist.item icon="document-check" :href="route('pm.clients')" :current="request()->routeIs('pm.clients')" wire:navigate>Clients</flux:navlist.item>
+                        <flux:navlist.item icon="document-text" :href="route('pm.invoices')" :current="request()->routeIs('pm.invoices')" wire:navigate>Invoices</flux:navlist.item>
+                    </flux:navlist.group>
+                </flux:navlist>
+            </flux:navlist.group>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="folder-git-2" href="https://github.com/zachran-recodex/recodex.git" target="_blank">
+                    Repository
+                </flux:navlist.item>
+            </flux:navlist>
+
+            <!-- Desktop User Menu -->
+            <flux:dropdown position="bottom" align="start">
+                <flux:profile
+                    name="Testing"
+                    initials="T"
+                    icon-trailing="chevrons-up-down"
+                />
+
+                <flux:menu class="w-[220px]">
+                    <flux:menu.radio.group>
+                        <div class="p-0 text-sm font-normal">
+                            <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
+                                    >
+                                        T
+                                    </span>
+                                </span>
+
+                                <div class="grid flex-1 text-start text-sm leading-tight">
+                                    <span class="truncate font-semibold">Testing</span>
+                                    <span class="truncate text-xs">testing@mail.com</span>
+                                </div>
+                            </div>
+                        </div>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>Settings</flux:menu.item>
+                    </flux:menu.radio.group>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:sidebar>
+
+        <!-- Mobile User Menu -->
+        <flux:header class="lg:hidden">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
+
+            <flux:spacer />
+
+            <flux:dropdown position="top" align="end">
+                <flux:profile
+                    :initials="T"
+                    icon-trailing="chevron-down"
+                />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <div class="p-0 text-sm font-normal">
+                            <div class="flex items-center gap-2 px-1 py-1.5 text-start text-sm">
+                                <span class="relative flex h-8 w-8 shrink-0 overflow-hidden rounded-lg">
+                                    <span
+                                        class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
+                                    >
+                                        T
+                                    </span>
+                                </span>
+
+                                <div class="grid flex-1 text-start text-sm leading-tight">
+                                    <span class="truncate font-semibold">Testing</span>
+                                    <span class="truncate text-xs">testing@mail.com</span>
+                                </div>
+                            </div>
+                        </div>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>Settings</flux:menu.item>
+                    </flux:menu.radio.group>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:header>
+
+        <flux:main>
+            {{ $slot }}
+        </flux:main>
+
+        @fluxScripts
+    </body>
+</html>
