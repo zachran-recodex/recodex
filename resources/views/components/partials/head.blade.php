e172dd515fbc0acdfe20ac1dff663abd3dac094d
Zachran Razendra <zachranraze@recodex.id>
2025-05-09 09:21:25 +0700

    feat(views): add head partial component for consistent metadata
    Introduce a new Blade partial component to centralize metadata and favicon configurations across the application. This ensures consistency and simplifies future updates to the head section of the application's views.

---
+++ resources/views/components/partials/head.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png', true) }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png', true) }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png', true) }}">
+<link rel="shortcut icon" href="{{ asset('favicon.ico', true) }}" type="image/x-icon">
+<link rel="manifest" href="{{ asset('site.webmanifest', true) }}">
+
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+@fluxAppearance
